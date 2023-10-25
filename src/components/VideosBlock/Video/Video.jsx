cb31bd80da7a7329c52208087c00af3e32ab68d2
EvgenijRyabko <jeniaryabko@bk.ru>
2023-10-25 20:38:45 +0300

    VideoBLock_changes

--- src/components/VideosBlock/Video/Video.jsx
+++ src/components/VideosBlock/Video/Video.jsx
@@ -1,18 +1,33 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import classes from "./Video.module.css";
 
 const Video = ({ text }) => {
-  return <div className={classes.video_block}>
-    <video src={text} controls className="w-[407px] h-[230px]"/>
-    {/* <a href="">
-        <svg xmlns="https://www.svgrepo.com/show/533682/download.svg"></svg>
-        mit Untertitel
-      </a>
-      <a href="">
-        <svg xmlns="https://www.svgrepo.com/show/533682/download.svg"></svg>
-        ohne Untertitel
-      </a> */}
-  </div>;
+  return (
+    <div className={classes.video_block}>
+      <video src={text} controls className="w-[407px] h-[230px]" />
+      <div className="grid grid-cols-2">
+        <div className="grid">
+          <button className="grid grid-cols-[2fr_6fr]">
+            <img
+              className="w-full"
+              src="https://www.svgrepo.com/show/533682/download.svg"
+            />
+            <p>mit Untertitel</p>
+          </button>
+        </div>
+        <div>
+          <button className="grid grid-cols-[2fr_6fr]">
+            <img
+              className="w-full"
+              src="https://www.svgrepo.com/show/533682/download.svg"
+            />
+            <p>ohne Untertitel</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Video;
